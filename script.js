804e80b343ebdd49bb44e34e7ed0a96036415668
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-10-25 17:23:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 //13. Write a JavaScript functionality to add a new link into the navbar
 
-const navBar = document.querySelector(
-  "nav.nav.d-flex.justify-content-between.mb-2"
-);
+function addNewLinkInNavBar() {
+  const navBar = document.querySelector(
+    "nav.nav.d-flex.justify-content-between.mb-2"
+  );
 
-const newLink = document.createElement("span");
-newLink.innerText = "New link added";
+  const newLink = document.createElement("a");
+  newLink.setAttribute("href", "https://www.google.com");
+  newLink.innerText = "New link added";
 
-navBar.appendChild(newLink);
+  navBar.appendChild(newLink);
+}
+
+addNewLinkInNavBar();
 
 //14. Write a JavaScript functionality to change the color of the main heading title
 
-const heading = document.querySelector(
-  "main.container div.row div.col-md-8.blog-main h2:nth-of-type(2)"
-);
+const changeColorOfMainHeading = function () {
+  const heading = document.querySelector(
+    "main.container div.row div.col-md-8.blog-main h2:nth-of-type(2)"
+  );
 
-heading.style.color = "darkgreen";
+  const mainHeading = document.querySelector("h1");
+  mainHeading.style.color = "darkgreen";
+
+  heading.style.color = "darkgreen";
+};
+
+changeColorOfMainHeading();
 
 //15. Write a JavaScript functionality to change the background of the jumbotron
 
@@ -26,51 +38,63 @@ jumbotron.style.backgroundColor = "yellowgreen";
 //16. Write a JavaScript functionality to remove the “twitter” link under the “Elsewhere”
 //    section in the aside element. It should happen when the page loads, automatically
 
-const linkToRemove = document.querySelector(
-  "aside.col-md-4.blog-sidebar div.p-4:nth-of-type(3) ol.list-unstyled li:nth-child(2) a"
-);
+function removeTwitterLink() {
+  const linkToRemove = document.querySelector(
+    "aside.col-md-4.blog-sidebar div.p-4:nth-of-type(3) ol.list-unstyled li:nth-child(2) a"
+  );
+  linkToRemove.remove();
+}
 
 window.onload = () => {
-  linkToRemove.remove();
+  removeTwitterLink();
 };
 
 //17. Write a JavaScript functionality to remove the first 50 characters in the first paragraph for every blog post
 
-const blogPostList = document.querySelectorAll(
-  "div.blog-post p:nth-of-type(2)"
-);
+const removeFirst50 = function () {
+  const blogPostList = document.querySelectorAll(
+    "div.blog-post p:nth-of-type(2)"
+  );
 
-for (let i = 0; i < blogPostList.length; i++) {
-  // console.log(blogPostList[i]);
-  const str = blogPostList[i].innerText;
-  console.log(str);
-  const firstFiftyCharacters = str.substring(50);
-  console.log(firstFiftyCharacters);
-}
+  for (let i = 0; i < blogPostList.length; i++) {
+    // console.log(blogPostList[i]);
+    let str = blogPostList[i].innerText;
+    blogPostList[i].style.color = "red";
+    console.log(str);
+    const firstFiftyCharacters = str.substring(50);
+    blogPostList[i].innerText = firstFiftyCharacters;
+  }
+};
+
+removeFirst50();
 
 //18. Write a JavaScript functionality to add a new Blog Post (div with title and text)
 
-const parent = document.querySelector(
-  "main.container div.row div.col-md-8.blog-main div.blog-post:nth-of-type(3)"
-);
+function addNewBlogPost() {
+  const parent = document.querySelector(
+    "main.container div.row div.col-md-8.blog-main div.blog-post:nth-of-type(3)"
+  );
 
-parent.style.backgroundColor = "greenyellow";
-const newBlogPost = document.createElement("div");
-// newBlogPost.classList.add("blog-post");
-newBlogPost.style.backgroundColor = "lavender";
+  parent.style.backgroundColor = "greenyellow";
+  const newBlogPost = document.createElement("div");
+  // newBlogPost.classList.add("blog-post");
+  newBlogPost.style.backgroundColor = "lavender";
 
-const newTitle = document.createElement("h2");
-newTitle.classList.add("blog-post-title");
-newTitle.innerText = "My new Blog post";
+  const newTitle = document.createElement("h2");
+  newTitle.classList.add("blog-post-title");
+  newTitle.innerText = "My new Blog post";
 
-const newText = document.createElement("div");
-newText.innerText =
-  "Some text for my new blog post. I have just created it using javascript. I think it looks very nice.";
+  const newText = document.createElement("div");
+  newText.innerText =
+    "Some text for my new blog post. I have just created it using javascript. I think it looks very nice.";
 
-newBlogPost.appendChild(newTitle);
-newBlogPost.appendChild(newText);
+  newBlogPost.appendChild(newTitle);
+  newBlogPost.appendChild(newText);
 
-parent.after(newBlogPost);
+  parent.after(newBlogPost);
+}
+
+addNewBlogPost();
 
 //19. Write a JavaScript functionality to remove the last Blog Post when the user clicks on the “Older” button
 
@@ -79,7 +103,7 @@ const olderButton = document.querySelector(
 );
 
 const lastBlogPost = document.querySelector(
-  "main.container div.row div.col-md-8.blog-main div.blog-post:nth-of-type(3)"
+  "div.col-md-8.blog-main div:nth-of-type(4)"
 );
 
 olderButton.addEventListener("click", () => {
@@ -97,7 +121,7 @@ const authorsList = document.querySelectorAll(
 
 for (let i = 0; i < authorsList.length; i++) {
   authorsList[i].addEventListener("mouseover", () => {
-    alert("author hovered");
+    alert(`${authorsList[i].innerText}`);
   });
 }
 
